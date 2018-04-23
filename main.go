8b41960cdb84d0d0bb25c086b9d9e9208e831789
Sucipto <show.cheap@gmail.com>
2018-04-23 10:31:18 +0700

    Add mux router

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := mux.NewRouter().StrictSlash(true)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln(w, "Welcome to ToDo App")
+
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+
+	todos := Todos{
+		Todo{Name: "Write Presentation"},
+		Todo{Name: "Host Meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoID := vars["todoId"]
+
+	fmt.Fprintln(w, "Todo Show", todoID)
 }
