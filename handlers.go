03c86ee94cb787f2c23cb701d0b17b27d1e4d065
Sucipto <show.cheap@gmail.com>
2018-04-23 10:54:21 +0700

    Add response header & code

--- handlers.go
+++ handlers.go
@@ -18,7 +18,12 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		Todo{Name: "Host Meetup"},
 	}
 
-	json.NewEncoder(w).Encode(todos)
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		panic(err)
+	}
 }
 
 func TodoShow(w http.ResponseWriter, r *http.Request) {
